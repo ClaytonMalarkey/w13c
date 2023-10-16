fb25f286b0997ed76c35b715ab514c15169e42d2
ClaytonMalarkey <malarkey1985@gmail.com>
2023-10-15 23:30:55 -0600

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="song-list">
+      <div v-for="song in songs" :key="song.song_id" @click="selectSong(song)">
+        <img :src="song.image_url" alt="Album Art" />
+        <p>{{ song.title }}</p>
+        <p>{{ song.artist }}</p>
+      </div>
+    </div>
+    <div class="playing-song">
+      <SongDisplay :selectedSong="selectedSong" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SongDisplay from "./components/SongDisplay.vue";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      songs: [
+        {
+          title: "Song 1",
+          artist: "Artist 1",
+          song_id: 1,
+          image_url: "image_url_1.jpg",
+        },
+        {
+          title: "Song 2",
+          artist: "Artist 2",
+          song_id: 2,
+          image_url: "image_url_2.jpg",
+        },
+        // Add more songs here
+      ],
+      selectedSong: null,
+    };
+  },
+  methods: {
+    selectSong(song) {
+      this.selectedSong = song;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    SongDisplay,
+  },
+};
 </script>
+<style scoped>
+.song-list {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.song-item {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  cursor: pointer;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f7f7f7;
+  transition: background-color 0.3s;
+}
+
+.song-item:hover {
+  background-color: #e2e2e2;
+}
+
+.album-art {
+  max-width: 100px;
+  max-height: 100px;
+}
+
+.song-title {
+  font-weight: bold;
+}
+
+.song-artist {
+  color: #555;
 }
 </style>
---
+++ src/components/SongDisplay.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="song-display">
+      <h2>Playing Song</h2>
+      <div v-if="selectedSong" class="playing-song-info">
+        <img :src="selectedSong.image_url" alt="Album Art" class="album-art" />
+        <p class="song-title">{{ selectedSong.title }}</p>
+        <p class="song-artist">{{ selectedSong.artist }}</p>
+      </div>
+      <div v-else>
+        <p class="no-song-selected">No song selected. Please pick a song.</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      selectedSong: Object,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .song-display {
+    text-align: center;
+  }
+  
+  .playing-song-info {
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f7f7f7;
+  }
+  
+  .album-art {
+    max-width: 150px;
+    max-height: 150px;
+  }
+  
+  .song-title {
+    font-weight: bold;
+  }
+  
+  .song-artist {
+    color: #555;
+  }
+  
+  .no-song-selected {
+    font-style: italic;
+    color: #888;
+  }
+  </style>
+  
